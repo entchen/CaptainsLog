2b356c35d734db5ca5c7b57ab6f70b94b4112dbf
Hakan Tandogan <hakan@gurkensalat.com>
2015-01-14 18:40:51 +0100

    And to ubiquitous Util class with a logger in it

---
+++ source/SCANsat-Notebook/NotebookUtil.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace SCANsatNotebook
+{
+	public class NotebookUtil
+	{
+		public NotebookUtil ()
+		{
+		}
+
+		public static void NotebookLog(string log, params object[] stringObjects)
+		{
+			log = string.Format(log, stringObjects);
+			string finalLog = string.Format("[SCANsat-Notebook] {0}", log);
+			Debug.Log(finalLog);
+		}
+	}
+}
+
