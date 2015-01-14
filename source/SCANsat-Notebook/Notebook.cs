890b0f61e27f66ac147c850c1955d5b4c6d14565
Hakan Tandogan <hakan@gurkensalat.com>
2015-01-14 13:23:19 +0100

    Every Saga Has A Beginning...

---
+++ source/SCANsat-Notebook/Notebook.cs
@@ -0,0 +1,19 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using UnityEngine;
+using KSP;
+
+namespace SCANsatNotebook
+{
+	[KSPAddon(KSPAddon.Startup.MainMenu, true)]
+	public class Notebook : MonoBehaviour
+	{
+		public Notebook ()
+		{
+		}
+	}
+}
+
