7f6cced9fdccbc0b7e1c89575bd7e703c082e450
Hakan Tandogan <hakan@gurkensalat.com>
2015-01-14 13:41:00 +0100

    A Scenario Controller class seems to be required

---
+++ source/SCANsat-Notebook/NotebookController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SCANsatNotebook
+{
+	[KSPScenario(ScenarioCreationOptions.AddToAllGames | ScenarioCreationOptions.AddToExistingGames, GameScenes.FLIGHT, GameScenes.SPACECENTER, GameScenes.TRACKSTATION)]
+	public class NotebookController
+	{
+		public NotebookController ()
+		{
+		}
+
+		public override void OnSave(ConfigNode node)
+		{
+		}
+	}
+}
+
