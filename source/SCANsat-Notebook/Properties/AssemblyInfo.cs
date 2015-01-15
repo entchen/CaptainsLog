b4b103168cabe25a6b55d8ca97c6d38aee7e71b5
Hakan Tandogan <hakan@gurkensalat.com>
2015-01-15 12:28:34 +0100

    Add AssemblyInfo to project

---
+++ source/SCANsat-Notebook/Properties/AssemblyInfo.cs
@@ -0,0 +1,28 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Information about this assembly is defined by the following attributes.
+// Change them to the values specific to your project.
+[assembly: AssemblyTitle ("SCANsat-Notebook")]
+[assembly: AssemblyDescription ("")]
+[assembly: AssemblyConfiguration ("")]
+[assembly: AssemblyCompany ("")]
+[assembly: AssemblyProduct ("")]
+[assembly: AssemblyCopyright ("")]
+[assembly: AssemblyTrademark ("")]
+[assembly: AssemblyCulture ("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("7c8daa00-adcb-485f-ab76-9d6d02e96861")]
+
+// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
+// The form "{Major}.{Minor}.*" will automatically update the build and revision,
+// and "{Major}.{Minor}.{Build}.*" will update just the revision.
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
+// [assembly: AssemblyInformationalVersion ("v9rc4.1")]
+[assembly: KSPAssembly("SCANsat-Notebook", 1, 0)]
+[assembly: KSPAssemblyDependency("SCANsat", 0, 9)]
+
+
