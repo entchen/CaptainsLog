2652fd89ad6136ea3d1e12220c3a9f67726950e9
Hakan Tandogan <hakan@gurkensalat.com>
2015-01-22 20:05:11 +0100

    Ongoing experiments to obtain SCANdata

--- source/SCANsat-Notebook/NotebookController.cs
+++ source/SCANsat-Notebook/NotebookController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using SCANsat;
+using SCANsat.SCAN_Data;
 
 namespace SCANsatNotebook
 {
@@ -32,36 +33,53 @@ namespace SCANsatNotebook
 			NotebookUtil.NotebookLog ("Checking {0}", name);
 
 			CelestialBody body = new CelestialBody ();
-			body.bodyName = name;
 
 			NotebookUtil.NotebookLog ("Created Celestial Body {0}", name);
-			NotebookUtil.NotebookLog ("  -- {0}", body.bodyName);
-			// SCANdata data = SCANUtil.getData (body);
-			//
-			// SCANdata.SCANanomaly[] anomalies = data.Anomalies;
-			// if (anomalies == null)
-			// {
-				// NotebookUtil.NotebookLog ("No known anomalies for: {0}", body.bodyName);
-			// }
-			// else
-			// {
-				// NotebookUtil.NotebookLog ("Body {0}: {1} known anomalies", new object[2] { body.bodyName, anomalies.Length});
-				// for (int i = 0; i < anomalies.Length; i++)
-				// {
-					// SCANdata.SCANanomaly anomaly = anomalies [i];
-					// internal bool known;
-					// internal bool detail;
-					// internal string name;
-					// internal double longitude;
-					// internal double latitude;
-					// internal PQSMod mod;
-					// NotebookUtil.NotebookLog ("    name:   {0}", anomaly.Name);
-					// NotebookUtil.NotebookLog ("    known:  {0}", anomaly.Known);
-					// NotebookUtil.NotebookLog ("    detail: {0}", anomaly.Detail);
-					// NotebookUtil.NotebookLog ("    lon:    {0}", anomaly.Longitude);
-					// NotebookUtil.NotebookLog ("    lat:    {0}", anomaly.Latitude);
-				// }
-			// }
+			if (body == null)
+			{
+				NotebookUtil.NotebookLog ("  -- no body for: {0}: '{1}'", new Object[] { name, body });
+			}
+			else
+			{
+				body.bodyName = name;
+				NotebookUtil.NotebookLog ("  -- body for: {0}: '{1}'", new Object[] { name, body });
+
+				SCANdata data = SCANUtil.getData (body);
+				NotebookUtil.NotebookLog ("  -- data - {0}", data);
+
+				if (data == null)
+				{
+					NotebookUtil.NotebookLog ("    -- no data for: {0}", body.bodyName);
+				}
+				else
+				{
+					NotebookUtil.NotebookLog ("    -- data for: {0}: '{1}'", new Object[] { body.bodyName, data });
+					SCANanomaly[] anomalies = data.Anomalies;
+					if (anomalies == null)
+					{
+						NotebookUtil.NotebookLog ("      -- no anomalies for: {0}", body.bodyName);
+					}
+					else
+					{
+						NotebookUtil.NotebookLog ("      -- {1} anomalies for: {0}", new Object[] { body.bodyName, anomalies.Length });
+						// NotebookUtil.NotebookLog ("Body {0}: {1} known anomalies", new object[2] { body.bodyName, anomalies.Length});
+						// for (int i = 0; i < anomalies.Length; i++)
+						// {
+						// SCANdata.SCANanomaly anomaly = anomalies [i];
+						// internal bool known;
+						// internal bool detail;
+						// internal string name;
+						// internal double longitude;
+						// internal double latitude;
+						// internal PQSMod mod;
+						// NotebookUtil.NotebookLog ("    name:   {0}", anomaly.Name);
+						// NotebookUtil.NotebookLog ("    known:  {0}", anomaly.Known);
+						// NotebookUtil.NotebookLog ("    detail: {0}", anomaly.Detail);
+						// NotebookUtil.NotebookLog ("    lon:    {0}", anomaly.Longitude);
+						// NotebookUtil.NotebookLog ("    lat:    {0}", anomaly.Latitude);
+					}
+				}
+			}
 		}
 	}
 }
