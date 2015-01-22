a780498abcdacb0db1b7b021f79ce2dc94f92418
Hakan Tandogan <hakan@gurkensalat.com>
2015-01-22 18:10:41 +0100

    The dll goes into the Plugins directory

--- source/SCANsat-Notebook/relaunch-ksp.cmd
+++ source/SCANsat-Notebook/relaunch-ksp.cmd
@@ -4,6 +4,6 @@ set KSP="d:\SteamLibrary\SteamApps\common\Kerbal Space Program.0.90.min"
 
 if not exist %KSP%\GameData\SCANsat-Notebook mkdir %KSP%\GameData\SCANsat-Notebook
 
-copy bin\debug\SCANsat-Notebook.dll %KSP%\GameData\SCANsat-Notebook
+copy bin\debug\SCANsat-Notebook.dll %KSP%\GameData\SCANsat-Notebook\Plugins
 
 endlocal
