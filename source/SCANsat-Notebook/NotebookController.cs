2702342d3a05d62abc45bdfe69fc13dadc1c517e
Hakan Tandogan <hakan@gurkensalat.com>
2015-01-24 16:07:17 +0100

    Access to the Anomaly data works now

--- source/SCANsat-Notebook/NotebookController.cs
+++ source/SCANsat-Notebook/NotebookController.cs
@@ -30,8 +30,6 @@ namespace SCANsatNotebook
 			{
 				foreach (CelestialBody body in bodies)
 				{
-					NotebookUtil.NotebookLog ("  body - name: {0}", body.bodyName);
-					NotebookUtil.NotebookLog ("  body - {0}", body);
 					SaveBody (body);
 				}
 			}
@@ -45,42 +43,39 @@ namespace SCANsatNotebook
 			}
 			else
 			{
-				body.bodyName = name;
-				NotebookUtil.NotebookLog ("  -- body for: {0}: '{1}'", new Object[] { name, body });
-
 				SCANdata data = SCANUtil.getData (body);
 				NotebookUtil.NotebookLog ("  -- data - {0}", data);
 
 				if (data == null)
 				{
-					NotebookUtil.NotebookLog ("    -- no data for: {0}", body.bodyName);
+					NotebookUtil.NotebookLog ("    -- no data for: {0}", body.name);
 				}
 				else
 				{
-					NotebookUtil.NotebookLog ("    -- data for: {0}: '{1}'", new Object[] { body.bodyName, data });
 					SCANanomaly[] anomalies = data.Anomalies;
 					if (anomalies == null)
 					{
-						NotebookUtil.NotebookLog ("      -- no anomalies for: {0}", body.bodyName);
+						NotebookUtil.NotebookLog ("      -- no anomalies for: {0}", body.name);
 					}
 					else
 					{
-						NotebookUtil.NotebookLog ("      -- {1} anomalies for: {0}", new Object[] { body.bodyName, anomalies.Length });
+						NotebookUtil.NotebookLog ("      -- {1} anomalies for: {0}", new Object[] { body.name, anomalies.Length });
 						// NotebookUtil.NotebookLog ("Body {0}: {1} known anomalies", new object[2] { body.bodyName, anomalies.Length});
-						// for (int i = 0; i < anomalies.Length; i++)
-						// {
-						// SCANdata.SCANanomaly anomaly = anomalies [i];
-						// internal bool known;
-						// internal bool detail;
-						// internal string name;
-						// internal double longitude;
-						// internal double latitude;
-						// internal PQSMod mod;
-						// NotebookUtil.NotebookLog ("    name:   {0}", anomaly.Name);
-						// NotebookUtil.NotebookLog ("    known:  {0}", anomaly.Known);
-						// NotebookUtil.NotebookLog ("    detail: {0}", anomaly.Detail);
-						// NotebookUtil.NotebookLog ("    lon:    {0}", anomaly.Longitude);
-						// NotebookUtil.NotebookLog ("    lat:    {0}", anomaly.Latitude);
+						for (int i = 0; i < anomalies.Length; i++)
+						{
+							SCANanomaly anomaly = anomalies [i];
+							// internal bool known;
+							// internal bool detail;
+							// internal string name;
+							// internal double longitude;
+							// internal double latitude;
+							// internal PQSMod mod;
+							NotebookUtil.NotebookLog ("    name:   {0}", anomaly.Name);
+							NotebookUtil.NotebookLog ("    known:  {0}", anomaly.Known);
+							NotebookUtil.NotebookLog ("    detail: {0}", anomaly.Detail);
+							NotebookUtil.NotebookLog ("    lon:    {0}", anomaly.Longitude);
+							NotebookUtil.NotebookLog ("    lat:    {0}", anomaly.Latitude);
+						}
 					}
 				}
 			}
