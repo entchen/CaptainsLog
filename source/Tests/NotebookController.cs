e7fc4dc51c1b23b57463c6fea771ebf47e1d3410
Hakan Tandogan <hakan@gurkensalat.com>
2015-01-28 19:01:41 +0100

    Hello World test case

---
+++ source/Tests/NotebookController.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+using NUnit.Framework;
+
+namespace Tests
+{
+	[TestFixture]
+	public class NotebookController
+	{
+		[Test]
+		[Category("sumfink")]
+		public void SezHelloWorld()
+		{
+		}
+	}
+}
+
